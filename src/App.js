b97e63d0e0c42908f9132c961a39ebc7897902e9
Olga Verbetska <verbetska150@gmail.com>
2021-04-08 20:01:11 +0300

    create work with Local Storage

--- src/App.js
+++ src/App.js
@@ -6,17 +6,50 @@ import Filter from './components/Filter';
 
 import './styles.css';
 
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: initialContacts,
 
     filter: '',
   };
+  //==================================================================
+  // сохранение в LocalStorage
+  componentDidMount() {
+    console.log('did mount');
+    const myContacts = localStorage.getItem('My Contacts');
+    const parsedMyContacts = JSON.parse(myContacts);
+    // if (parsedMytodos.length === 0) {
+    //   this.setState({
+    //     todos: initialTodos,
+    //   });
+    // }
+
+    // если в тудус пусто [], то
+    if (parsedMyContacts) {
+      this.setState({
+        contacts: parsedMyContacts,
+      });
+    } else {
+      this.setState({
+        contacts: initialContacts,
+      });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    //  здесь можно вызвать сет Стейт только по проверке условия
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('My Contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  //==================================================================
 
   addContact = ({ name, number }) => {
     const contactData = {
